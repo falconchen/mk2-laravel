0e9a8cea20bc33695fe3423ea059ba6521609078
falcon <falcon_chen@qq.com>
2024-01-26 18:23:20 +0800

    add mail address text

--- resources/views/livewire/ebook/upload-ebook.blade.php
+++ resources/views/livewire/ebook/upload-ebook.blade.php
@@ -1,43 +1,53 @@
+
 <div class="">
 
 
-    <form class="space-y-8 md:w-1/2 px-4 py-8 bg-white rounded  mt-8 mx-auto" wire:submit.prevent="submit"
-        x-data="{ uploading: false, progress: 0 ,fail: false}"
-        x-on:livewire-upload-start="uploading = true"
-        x-on:livewire-upload-finish="uploading = false"
-        x-on:livewire-upload-error="uploading = false; fail = true"
-        x-on:livewire-upload-progress="progress = $event.detail.progress"
+    <form class="px-4 py-8 mx-auto mt-8 space-y-8 bg-white rounded md:w-1/2" wire:submit.prevent="submit"
+        x-data="{ uploading: false, progress: 0, fail: false }" x-on:livewire-upload-start="uploading = true"
+        x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-error="uploading = false; fail = true"
+        x-on:livewire-upload-progress="progress = $event.detail.progress">
 
-    >
 
-        <h1 class="text-center font-bold text-2xl">Upload Your Ebook</h1>
+        <h1 class="text-2xl font-bold text-center">Upload Your Ebook</h1>
+
+        <details class="px-1 py-2 ">
+            <summary ><p class="inline font-bold text-red-700">❗️important<p/>
+                First of all, You need to add
+                <code class="inline-block px-0 py-0 text-blue-700 rounded">
+                    {{ config('mail.from.address', 'hello@example.com') }}
+                </code> to the <a class="underline hover:no-underline" target="_blank" href="https://www.amazon.com/hz/mycd/myx#/home/settings/payment"">Approved Personal Document E-mail List.</a>
+            </summary>
+            <p>
+                <img src="https://photo.cellmean.com/i/2024/01/26/txu15t-0.png" alt="add to your kindle Approved Personal Document E-mail List"/>
+            </p>
+        </details>
+
 
         @if (session()->has('message'))
-        <div class="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
-            <p class="font-bold">Success</p>
-            <p>{{ session('message') }}</p>
-        </div>
+            <div class="p-4 text-blue-700 bg-blue-100 border-l-4 border-blue-500" role="alert">
+                <p class="font-bold">Success</p>
+                <p>{{ session('message') }}</p>
+            </div>
         @endif
 
         @if (session()->has('error'))
-        <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
-            <p class="font-bold">Error</p>
-            <p>{{ session('error') }}</p>
-        </div>
+            <div class="p-4 text-red-700 bg-red-100 border-l-4 border-red-500" role="alert">
+                <p class="font-bold">Error</p>
+                <p>{{ session('error') }}</p>
+            </div>
         @endif
 
         <div>
-            <label for="kindle_email" class="block text-sm font-medium text-gray-700 leading-5">
+            <label for="kindle_email" class="block text-sm font-medium leading-5 text-gray-700">
                 Your Sent To Kindle Email Address
 
 
             </label>
 
             <div class="mt-1 rounded-md shadow-sm">
-                <input wire:model.lazy="kindle_email"
-                id="kindle_email"
-                type="email"
-                required autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 " placeholder="example@kindle.com" />
+                <input wire:model.lazy="kindle_email" id="kindle_email" type="email" required autofocus
+                    class="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-blue focus:border-blue-300 sm:text-sm sm:leading-5 "
+                    placeholder="example@kindle.com" />
             </div>
 
         </div>
@@ -47,12 +57,11 @@
 
 
 
-        <div class="bg-gray-50 p-4 border-2 border-dashed border-gray-300 rounded-md space-y-2">
+        <div class="p-4 space-y-2 border-2 border-gray-300 border-dashed rounded-md bg-gray-50">
 
             <div class="w-full h-24 flex items-center justify-center cursor-pointer {{ isset($ebook) ? 'border-none' : 'border-2 border-dashed' }}"
-                @if ( $ebook ) wire:click="resetEbook" @endif
-                @click.native="$refs.fileInput.click();progress=0"
-            >
+                @if ($ebook) wire:click="resetEbook" @endif
+                @click.native="$refs.fileInput.click();progress=0">
 
 
 
@@ -60,28 +69,26 @@
                 <div class="flex flex-col text-center ">
 
                     @if (isset($ebook))
-
                         <strong>
                             {{ $ebook->getClientOriginalName() }}
-                         </strong>
+                        </strong>
                         {{-- // Available file properties.
                         $fileOriginalName = $this->file->getClientOriginalName();
                         $fileTemporaryPath = $this->file->getRealPath();
                         $fileSize = $this->file->getSize();
                         $fileMimeType = $this->file->getMimeType();
                         $fileExtension = $this->file->getClientOriginalExtension(); --}}
-                        <p>{{ formatSizeUnits( $ebook->getSize()) }}</p>
+                        <p>{{ formatSizeUnits($ebook->getSize()) }}</p>
                     @else
                         <div x-show="!uploading">
-                        Click here to select a file
-                        <div x-show="fail">upload failed</div>
+                            Click here to select a file
+                            <div x-show="fail">upload failed</div>
                         </div>
                         <div x-show="uploading" style="display:none">
                             <div x-text="`${progress}%`"></div>
-                            <progress max="100"  x-bind:value="progress" x-show.transition.opacity="uploading"></progress>
+                            <progress max="100" x-bind:value="progress"
+                                x-show.transition.opacity="uploading"></progress>
                         </div>
-
-
                     @endif
 
 
@@ -90,15 +97,12 @@
         </div>
 
 
-        <button
-        class="w-full py-2 px-3 bg-blue-600 hover:bg-blue-700 text-white text-center rounded-md "
-        x-bind:disabled="uploading" x-bind:class="{' pointer-events-none opacity-50 cursor-not-allowed': uploading }"
-        type="submit">
+        <button class="w-full px-3 py-2 text-center text-white bg-blue-600 rounded-md hover:bg-blue-700 "
+            x-bind:disabled="uploading"
+            x-bind:class="{ ' pointer-events-none opacity-50 cursor-not-allowed': uploading }" type="submit">
 
             Submit
         </button>
 
     </form>
 </div>
-
-
